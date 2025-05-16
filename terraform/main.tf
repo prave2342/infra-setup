43ede9309d8fb90365931d5af4ef83dbb1e754c6
Praveen Ramachandran <prave2342@gmail.com>
2025-05-16 20:12:38 +0530

    Adding security groups

--- terraform/main.tf
+++ terraform/main.tf
@@ -192,10 +192,12 @@ resource "aws_eks_cluster" "eks-cluster" {
         subnet_ids              = aws_subnet.eks-subnet[*].id
         endpoint_private_access = true
         endpoint_public_access  = false
+        security_group_ids = [aws_security_group.jumpbox-nsg.id]
     }
     depends_on = [
         aws_iam_role_policy_attachment.eks-policy-attach,
-        aws_subnet.eks-subnet
+        aws_subnet.eks-subnet,
+        aws_security_group.jumpbox-nsg.
     ]
 }
 
@@ -204,7 +206,6 @@ resource "aws_eks_node_group" "eks_nodes" {
     node_group_name = "${var.cluster_name}-node-group"
     node_role_arn   = aws_iam_role.eks-node-role.arn
     subnet_ids      = aws_subnet.eks-subnet[*].id
-    vpc_security_group_ids = [aws_security_group.eks-node-nsg.id]
     scaling_config {
         desired_size = 1
         max_size     = 2
