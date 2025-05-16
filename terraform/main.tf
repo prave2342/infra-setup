e30b1c4a9414e231fa9ca74cee225c735292e3f4
prave2342 <prave2342@gmail.com>
2025-05-16 08:26:09 +0530

    Update main.tf
    correcting role name

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "aws_iam_role_policy_attachment" "eks-policy-attach" {
 }
 resource "aws_eks_cluster" "eks-cluster" {
     name     = var.cluster_name
-    role_arn = aws_iam_role.eks_cluster_role.arn
+    role_arn = aws_iam_role.eks-cluster-role.arn
     vpc_config {
         subnet_ids              = aws_subnet.eks-subnet.id
         endpoint_private_access = true
